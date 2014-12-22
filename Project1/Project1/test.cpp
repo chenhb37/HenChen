fe08f9285c598bc45f52f866f867d78533ca2ebe
chenhb37 <chenhb37@gmail.com>
2014-12-22 22:23:40 +0800

    作者：陈海宾

--- Project1/Project1/test.cpp
+++ Project1/Project1/test.cpp
@@ -1,11 +1,26 @@
+#include<stdio.h>
 #include<iostream>
 
 using namespace std;
 
+union Test{
+	unsigned int a;
+	char b[4];
+};
+
+/***
+时间去哪儿了
+*/
 int main()
 { 
-	cout<<"hello world"<<endl;
-	cout<<"I want to make some change"<<endl;
-	cout<<typeid(sizeof(int)).name()<<endl;
+	Test a;
+	a.b[0]=1;
+	a.b[1]=2;
+	a.b[2]=4;
+	a.b[3]=8;
+	printf("%d  %x\n",a.a,a.a);
+	printf("%x  %x",a.b[0],a.b[1]);
+    
+	system("pause");
 	return 0;
 }
